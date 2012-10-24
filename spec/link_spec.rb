b3764c2f21209aa12069eb5a0c293b3a6342ebc8
Ricardo Mendes <rokusu@gmail.com>
2012-10-24 06:53:28 +0100

    whitespace

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -7,19 +7,19 @@ describe Halibut::HAL::Link do
   describe "href" do
     it "accepts non-templated uri" do
       link = Halibut::HAL::Link.new normal_uri
-      
+
       link.templated?.must_equal false
       link.href.must_equal normal_uri
     end
-    
+
     it "accepts templated uri" do
       link = Halibut::HAL::Link.new tmpl_uri, true
-      
+
       link.templated?.must_equal true
       link.href.must_equal tmpl_uri
     end
   end
-  
+
   describe "optionals" do
     it "are set correctly" do
       link1 = Halibut::HAL::Link.new normal_uri, false, {
@@ -36,13 +36,13 @@ describe Halibut::HAL::Link do
         :title    => 'title',
         :hreflang => 'hreflang'
       }
-      
+
       link1.type.must_equal     "type"
       link1.name.must_equal     "name"
       link1.profile.must_equal  "profile"
       link1.title.must_equal    "title"
       link1.hreflang.must_equal "hreflang"
-      
+
       link2.type.must_equal     "type"
       link2.name.must_equal     "name"
       link2.profile.must_equal  "profile"
--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -3,78 +3,78 @@ require_relative 'spec_helper'
 describe Halibut::HAL::Resource do
   let(:templated_uri) { "http://example.com/{path}{?query}" }
   let(:normal_uri)    { "http://example.com" }
-  
+
   describe "Properties" do
     subject { Halibut::HAL::Resource.new }
-  
+
     it "set property" do
       subject.set_property "property", "value"
-      
+
       subject.properties['property'].must_equal "value"
     end
-    
+
     it "read property" do
       subject.set_property "property", "value"
-      
+
       subject.get_property('property').must_equal "value"
     end
   end
-  
+
   describe "Links" do
-  
+
     describe "self link" do
       it "no default" do
         resource = Halibut::HAL::Resource.new
         resource.links.must_be_empty
       end
-      
+
       it "default" do
         resource = Halibut::HAL::Resource.new normal_uri
-        
+
         resource.links.wont_be_empty
         resource.links['self'].first.href.must_equal normal_uri
       end
     end
-    
+
   end
-  
+
   describe "Embedded resources" do
     subject { Halibut::HAL::Resource.new }
     let(:res1) { Halibut::HAL::Resource.new "http://first-resource.com" }
     let(:res2) { Halibut::HAL::Resource.new "http://secnd-resource.com" }
-    
+
     it "no embedded resource" do
-      subject.embedded.must_be_empty 
+      subject.embedded.must_be_empty
     end
-    
+
     it "has embedded resource" do
       subject.embed_resource 'users', res1
       subject.embed_resource 'users', res2
-      
+
       subject.embedded['users'].first.must_equal res1
       subject.embedded['users'].last.must_equal  res2
     end
   end
-  
+
   describe "Serialize" do
     subject { Halibut::HAL::Resource.new("http://example.com").to_json }
-    
+
     it "serializes to JSON" do
       json = load_json "simple"
-      
+
       JSON.load(subject).must_equal JSON.load(json)
     end
   end
-  
+
   describe "Deserialize" do
     subject { Halibut::HAL::Resource.from_json(load_json "serialize") }
-    
+
     it "deserializes from JSON" do
       order = Halibut::HAL::Resource.new "/orders/123"
       order.set_property "total", 30.00
       order.set_property "currency", "USD"
       order.set_property "status", "shipped"
-      
+
       resource = Halibut::HAL::Resource.new "/orders"
       resource.add_link "find", "/orders{?id}", true
       resource.add_link "next", "/orders/1"
@@ -82,7 +82,7 @@ describe Halibut::HAL::Resource do
       resource.set_property "currentlyProcessing", 14
       resource.set_property "shippedToday", 20
       resource.embed_resource "orders", order
-      
+
       subject.must_equal resource
     end
   end
