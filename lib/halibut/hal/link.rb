2ff8b0f9b0b1672a93dfa5d37fa382887dc8b91c
Ricardo Mendes <rokusu@gmail.com>
2012-11-09 22:51:56 +0000

    extracted link options, seems cleaner

--- lib/halibut/hal/link.rb
+++ lib/halibut/hal/link.rb
@@ -1,24 +1,16 @@
 module Halibut::HAL
 
-  # This class represents a HAL Link object.
-  #
-  # spec spec spec.
-  class Link
-    attr_reader :href, :templated, :type, :name,
+  class Options
+    attr_reader :templated, :type, :name,
                 :profile, :title, :hreflang
 
-    # Returns an instance of a HAL Link object
-    #
-    # @param [String]  href      URI or URI Template
-    # @param [Boolean] templated true if URI Template or false otherwise
-    # @param [Hash]    opts      Options: type, name, profile, title, hreflang
-    #
-    # @return [Halibut::HAL::Link] HAL Link object
-    def initialize(href, templated=nil, opts={})
-      @href      = href
-      @templated = templated
-
-      set_options opts
+    def initialize opts
+      @templated = opts[:templated] || opts['templated']
+      @type      = opts[:type]      || opts['type']
+      @name      = opts[:name]      || opts['name']
+      @profile   = opts[:profile]   || opts['profile']
+      @title     = opts[:title]     || opts['title']
+      @hreflang  = opts[:hreflang]  || opts['hreflang']
     end
 
     # Returns whether the href is a templated uri or not.
@@ -30,9 +22,6 @@ module Halibut::HAL
       @templated || false
     end
 
-    # Returns a hash corresponding to the Link object.
-    #
-    # @return [Hash] hash from Link Object
     def to_hash
       a = instance_variables.each_with_object({}) do |name, output|
         next if (ivar = instance_variable_get(name)).nil?
@@ -40,29 +29,48 @@ module Halibut::HAL
         output[name[1..-1]] = ivar
       end
     end
+  end
+
+
+  # This class represents a HAL Link object.
+  #
+  # spec spec spec.
+  class Link
+    extend Forwardable
+
+    attr_reader :href
+
+    def_delegators :@options, :templated, :templated?, :type,
+                   :name, :profile, :title, :hreflang
+    # Returns an instance of a HAL Link object
+    #
+    # @param [String]  href      URI or URI Template
+    # @param [Boolean] templated true if URI Template or false otherwise
+    # @param [Hash]    opts      Options: type, name, profile, title, hreflang
+    #
+    # @return [Halibut::HAL::Link] HAL Link object
+    def initialize(href, opts={})
+      @href = href
+      @options = Options.new opts
+    end
+
+    # Returns a hash corresponding to the Link object.
+    #
+    # @return [Hash] hash from Link Object
+    def to_hash
+      { 'href' => href }.merge @options.to_hash
+    end
 
     def to_s
       @href
     end
 
     def ==(other)
-      @href      == other.href      &&
-      @templated == other.templated &&
-      @type      == other.type      &&
-      @name      == other.name      &&
-      @profile   == other.profile   &&
-      @title     == other.title     &&
-      @hreflang  == other.hreflang
+      @href == other.href #&& @options == other.options
     end
 
-    private
-    def set_options(opts)
-      @type     = opts[:type]
-      @name     = opts[:name]
-      @profile  = opts[:profile]
-      @title    = opts[:title]
-      @hreflang = opts[:hreflang]
-    end
+    protected
+    attr_reader :options
 
   end
 
