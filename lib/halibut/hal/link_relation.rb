c42854b99f894a72c06bfa6a291c418f64d0f14a
Ricardo Mendes <rokusu@gmail.com>
2012-11-09 22:53:44 +0000

    updated link spec, added LinkRelation which was removed meanwhile

---
+++ lib/halibut/hal/link_relation.rb
@@ -0,0 +1,29 @@
+module Halibut
+
+  # Domain object that represents a Relation
+  #
+  # spec spec spec.
+  class LinkRelation
+    attr_accessor :name, :curie
+
+    def initialize(name)
+      splits = name.to_s.split(":")
+
+      splits.size < 2 ? @name = splits.first : (@curie, @name = splits)
+    end
+
+    def eql?(other)
+      hash == other.hash
+    end
+
+    def hash
+      instance_variables.hash
+    end
+
+    def to_s
+      @curie and "#{@curie}:#{@name}" or @name
+    end
+
+  end
+
+end
---
+++ spec/link_relation_spec.rb
@@ -0,0 +1,41 @@
+require_relative 'spec_helper'
+
+describe Halibut::LinkRelation do
+
+  describe "String" do
+    it "has a name" do
+      relation = Halibut::LinkRelation.new 'default'
+
+      assert relation.name
+      relation.name.must_equal 'default'
+    end
+
+    it "has a curie and a name" do
+      relation = Halibut::LinkRelation.new 'mighty:max'
+
+      relation.curie.wont_be_nil
+      relation.curie.must_equal 'mighty'
+      relation.name
+      relation.name.must_equal 'max'
+    end
+  end
+
+  describe "Symbol" do
+    it "has a name" do
+      relation = Halibut::LinkRelation.new :default
+
+      assert relation.name
+      relation.name.must_equal 'default'
+    end
+
+    it "has a curie and a name" do
+      relation = Halibut::LinkRelation.new :'mighty:max'
+
+      relation.curie.wont_be_nil
+      relation.curie.must_equal 'mighty'
+      relation.name
+      relation.name.must_equal 'max'
+    end
+
+  end
+end
