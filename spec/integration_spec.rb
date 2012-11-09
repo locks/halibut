3b59688208a8912bdf6f46d7fe769bd93b6e7a97
Ricardo Mendes <rokusu@gmail.com>
2012-11-09 23:02:38 +0000

    tried refactoring to move the JSON stuff away from the Resource. it's not quite finished, but I think it's a step in the right direction

---
+++ spec/integration_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'spec_helper'
+
+require 'json'
+require 'hash'
+
+read_files = ->() {
+  Dir.tap {|it| it.chdir('spec/test-resources/src/main/resources') } \
+     .glob('*.json') \
+     .map {|f| File.read f }
+}
+
+describe Halibut do
+
+  it "tests against test-resources" do
+    files  = read_files[]
+
+    refilled  = files.map {|f| JSON.load f }
+    resources = files.map {|f| Halibut::HAL::Resource.from_json f }.map &:as_json
+
+    zipped = refilled.zip resources
+    zipped.each do |json, hal|
+      json.must_equal hal
+    end
+  end
+
+end
