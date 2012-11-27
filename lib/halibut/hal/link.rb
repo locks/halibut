a9c900327c092f0f8e87f9e4860a7b1fccb37a72
Ricardo Mendes <rokusu@gmail.com>
2012-11-27 01:40:05 +0000

    hide all the things.
    I'm still in my quest to expose stricly what's necessary to function.
    Yeah, I'm being rather zealous but you gotta do what you gotta do,
    amirite.

--- lib/halibut/hal/link.rb
+++ lib/halibut/hal/link.rb
@@ -1,37 +1,4 @@
 module Halibut::HAL
-
-  class Options
-    attr_reader :templated, :type, :name,
-                :profile, :title, :hreflang
-
-    def initialize opts
-      @templated = opts[:templated] || opts['templated']
-      @type      = opts[:type]      || opts['type']
-      @name      = opts[:name]      || opts['name']
-      @profile   = opts[:profile]   || opts['profile']
-      @title     = opts[:title]     || opts['title']
-      @hreflang  = opts[:hreflang]  || opts['hreflang']
-    end
-
-    # Returns whether the href is a templated uri or not.
-    #
-    # Since all instance variables except href can be nil,
-    #
-    # @return [true, false] whether the href is a templated uri or not.
-    def templated?
-      @templated || false
-    end
-
-    def to_hash
-      a = instance_variables.each_with_object({}) do |name, output|
-        next if (ivar = instance_variable_get(name)).nil?
-
-        output[name[1..-1]] = ivar
-      end
-    end
-  end
-
-
   # This class represents a HAL Link object.
   #
   # spec spec spec.
@@ -54,24 +21,61 @@ module Halibut::HAL
       @options = Options.new opts
     end
 
-    # Returns a hash corresponding to the Link object.
+    # Returns the Link as a Hash
     #
     # @return [Hash] hash from Link Object
     def to_hash
-      { 'href' => href }.merge @options.to_hash
-    end
-
-    def to_s
-      @href
+      { 'href' => href }.merge @options
     end
 
     def ==(other)
-      @href == other.href #&& @options == other.options
+      @href == other.href && @options == other.options
     end
 
     protected
     attr_reader :options
 
+    private
+    class Options
+      attr_reader :templated, :type, :name,
+                  :profile, :title, :hreflang
+
+      def initialize opts
+        @templated = opts[:templated] || opts['templated']
+        @type      = opts[:type]      || opts['type']
+        @name      = opts[:name]      || opts['name']
+        @profile   = opts[:profile]   || opts['profile']
+        @title     = opts[:title]     || opts['title']
+        @hreflang  = opts[:hreflang]  || opts['hreflang']
+      end
+
+      # Tells us if the href of the associated link is templated.
+      #
+      # The reason for not returning @templated directly is that all of the
+      # options are optional, thus nil could be returned instead of a boolean.
+      #
+      # @return [true, false] whether the href is a templated uri or not.
+      def templated?
+        @templated || false
+      end
+
+      def to_hash
+        instance_variables.each_with_object({}) do |name, output|
+          next if (ivar = instance_variable_get(name)).nil?
+
+          output[name[1..-1]] = ivar
+        end
+      end
+
+      def ==(other)
+        templated == other.templated &&
+        type      == other.type      &&
+        name      == other.name      &&
+        profile   == other.profile   &&
+        title     == other.title     &&
+        hreflang  == other.hreflang
+      end
+    end
   end
 
 end
