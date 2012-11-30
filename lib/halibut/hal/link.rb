65f079bf24d5bdda301281d8c2ac58c9d277b7d5
Ricardo Mendes <rokusu@gmail.com>
2012-11-30 22:14:30 +0000

    trying out a documentation style

--- lib/halibut/hal/link.rb
+++ lib/halibut/hal/link.rb
@@ -11,6 +11,17 @@ module Halibut::HAL
                    :name, :profile, :title, :hreflang
     # Returns an instance of a HAL Link object
     #
+    #     # link with no options
+    #     link = Link.new('http://homeopathy.org')
+    #
+    #     # link with name and type options
+    #     link = Link.new('http://homeopathy.org', name: 'Homeopath'
+    #                                            , type: 'text/html')
+    #
+    # If you pass an options that is not one of the reserved link properties
+    # as defined by the spec, they are dropped.
+    # It should possibly raise an error, hafta think about it.
+    #
     # @param [String]  href      URI or URI Template
     # @param [Boolean] templated true if URI Template or false otherwise
     # @param [Hash]    opts      Options: type, name, profile, title, hreflang
@@ -21,13 +32,28 @@ module Halibut::HAL
       @options = Options.new opts
     end
 
-    # Returns the Link as a Hash
+    # Simply returns a hash of the href and the options that are not empty.
+    #
+    #     link = Link.new('/links', name: 'Links')
+    #     link.to_hash
+    #     # => { "href" => "/links", "name" => "links" }
     #
     # @return [Hash] hash from Link Object
     def to_hash
       { 'href' => href }.merge @options
     end
 
+    # Generic comparison method.
+    #
+    # Two objects are the same if they have the same href and the same options.
+    #
+    #     link_one = Link.new('/link', name: 'One', type: 'text/html')
+    #     link_two = Link.new('/link', name: 'One', type: 'text/html')
+    #     link_one == link_two
+    #     # => true
+    #
+    # @param [Link] other Link object to compare to
+    # @return [true,false] return of the comparison of the two objects
     def ==(other)
       @href == other.href && @options == other.options
     end
@@ -36,6 +62,15 @@ module Halibut::HAL
     attr_reader :options
 
     private
+    # Options reifies the various optional properties of a link, as per the
+    # spec.
+    #
+    #     hash = { name: 'John le Carré', templated: true }
+    #     opts = Options.new(hash)
+    #     opts.name    # => John le Carré
+    #     opts['name'] # => John le Carré
+    #     opts[:name]  # => John le Carré
+    #
     class Options
       attr_reader :templated, :type, :name,
                   :profile, :title, :hreflang
@@ -59,14 +94,31 @@ module Halibut::HAL
         @templated || false
       end
 
+      # When converting to a hash, options that weren't set (.nil? == true) are
+      # kept out.
+      #
+      # This might have some implications, as it does not 'serialiaze' options
+      # that were explicitely set to nil. On the other hand, one can argue that
+      # if they were explicitly set to nil, then they shouldn't show up anyway.
       def to_hash
         instance_variables.each_with_object({}) do |name, output|
-          next if (ivar = instance_variable_get(name)).nil?
+          ivar = ivar = instance_variable_get(name)
+
+          next if ivar.nil?
 
           output[name[1..-1]] = ivar
         end
       end
 
+      # Straight forward comparison between two Options objects.
+      #
+      #     opts_one = Options.new(name: 'Link', templated: true)
+      #     opts_two = Options.new(name: 'Link', templated: true)
+      #     opts_one == opts_two
+      #     # => true
+      #
+      # @param [Options] other Options object to compare to.
+      # @return [true,false] whether these two objects are equivalent or not.
       def ==(other)
         templated == other.templated &&
         type      == other.type      &&
