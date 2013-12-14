b7cdbddbee4485909cc50727ae36ce0da68a1129
Ricardo Mendes <rokusu@gmail.com>
2013-12-14 21:26:12 +0000

    Replaces autoload with straight requires

--- lib/halibut.rb
+++ lib/halibut.rb
@@ -1,18 +1,17 @@
 require "halibut/version"
 
 # Halibut is the main namespace
-module Halibut
-  autoload :Builder,  'halibut/builder'
-  autoload :Document, 'halibut/document'
-end
+module Halibut; end
+
+require 'halibut/builder'
 
 # The Adapter namespace contains classes that aid in the
 # mapping of HAL Resources into a specific format.
-module Halibut::Adapter
-  autoload :JSON, 'halibut/adapter/json'
-end
+module Halibut::Adapter; end
+
+require 'halibut/adapter/json'
 
 # Halibut::Core contains the domain objects that reflect the HAL specs.
-module Halibut::Core
-  autoload :Resource, 'halibut/core/resource'
-end
+module Halibut::Core; end
+
+require 'halibut/core/resource'
--- lib/halibut/core.rb
+++ lib/halibut/core.rb
@@ -1,6 +1,7 @@
 module Halibut
   module Core
-    autoload :Resource, 'halibut/core/resource'
-    autoload :Link,     'halibut/core/link'
   end
 end
+
+require 'halibut/core/resource'
+require 'halibut/core/link'
